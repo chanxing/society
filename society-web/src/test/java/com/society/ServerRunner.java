0fc66249c268291214b426eec87a3b76043543fa
heyanggui <yanggui@wesdom.me>
2018-03-08 16:01:43 +0800

    tomcat文件跟路径

--- society-web/src/test/java/com/society/ServerRunner.java
+++ society-web/src/test/java/com/society/ServerRunner.java
@@ -7,6 +7,7 @@ public class ServerRunner {
 	public static void main(String[] args) {
 		// System.setProperty("ENV", "dev");
 		System.setProperty("server.port", "8088");
+		System.setProperty("server.tomcat.basedir", "/tmp/");
 		SpringApplication.run(SocietyServletInitializer.class, args);
 	}
 }
